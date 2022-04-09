dce1ba0971ca86e1e9ad89f13246d7d7570cc9d1
eichitakaya <takaya.wisdom0616@gmail.com>
2022-04-09 18:05:12 +0900

    developing

--- MedicalDataSets.py
+++ MedicalDataSets.py
@@ -1,3 +1,32 @@
 import glob
+import torch
 import numpy as np
-if 1 == 1:
+
+
+class SegmentationDecathlon(torch.utils.data.Dataset):
+    def __init__(self, name, traintest="train"):
+        super().__init__()
+
+        self.dataset_dir = {
+            "brain": "Task01_BrainTumour",
+            "heart": "Task02_Heart",
+            "liver": "Task03_Liver",
+            "hippocampus": "Task04_Hippocampus",
+            "prostate": "Task05_Prostate",
+            "lung": "Task06_Lung",
+            "pancreas": "Task07_Pancreas",
+            "hepaticvessel": "Task08_HepaticVessel",
+            "spleen": "Task09_Spleen",
+            "colon": "Task10_Colon"
+        }
+
+        # dataフォルダのあるディレクトリの絶対パス(コマンドライン引数にしてもいいかも？)
+        datafolder_path = "/takaya_workspace/data/decathlon"
+
+        self.dataset_path = datafolder_path + dataset_dir[name.lower()]
+
+        if traintest == "train":
+            print(0)
+
+    def __len__(self):
+        return 
