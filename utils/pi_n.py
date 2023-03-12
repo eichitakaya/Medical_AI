6291eea5222657c09ddd32f670fcdf31587524ad
eichitakaya <takaya.wisdom0616@gmail.com>
2023-03-12 15:23:27 +0900

    addd some notebooks

---
+++ utils/pi_n.py
@@ -0,0 +1,10 @@
+import math
+
+def pi_n(n):
+    #分子
+    numerator = (2 * (-1) ** (n+1)) * math.factorial(2*n)
+    
+    #分母
+    denominator = 
+    
+    return (numerator / denominator) ** (1 / (2 * n))
